343a55aca31a9dd8371ca3bfb5112cbff2c9a24c
Keyur Patel <patelkeyur2311@gmail.com>
2024-07-15 23:54:32 +0530

    site changed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://samvaad-9me6.onrender.com", {
+      const socket = io("https://samvaaad.netlify.app/", {
         query: {
           userId: authUser._id,
         },
