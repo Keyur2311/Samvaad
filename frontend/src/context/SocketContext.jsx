1cb37e89b0c267bb078af4a1fd2463ebec4907f2
Keyur Patel <patelkeyur2311@gmail.com>
2024-07-21 04:47:01 +0530

    fixex bug

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://samvaad-chat-app.vercel.app/", {
+      const socket = io("https://samvaad-9me6.onrender.com/", {
         query: {
           userId: authUser._id,
         },
